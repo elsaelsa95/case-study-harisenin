1194a9ac5b2e9f39d5194f0400959af0a2b3e9d3
elsaelsa95 <elsa.lustiana95@gmail.com>
2022-12-08 17:42:09 +0700

    router product

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 
 const routerAsset = require("./asset");
 const routerCategory = require("./category");
+const routerProduct = require("./product")
 
 router.use("/assets", routerAsset);
 
 router.use("/categories", routerCategory);
+
+router.use("/products", routerProduct)
 
 module.exports = router;
